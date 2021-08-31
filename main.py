60da93c583c0255615224557f04ca17daad47d46
Michael Folkson <michaelfolkson@gmail.com>
2021-08-31 15:03:17 +0100

    Add while loop to check Bitcoin price

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 #!/usr/bin/env python3
-
+"""Sends a text message when the Bitcoin price hits a certain threshold"""
 import requests
 import json
 import smsalert
 import secrets
-# import importlib
-
-# importlib.import_module(smsalert)
+import time
+import sys
 
 def checksite():
+    """Requests various Bitcoin price data from Bitfinex"""
     response = requests.get("https://api.bitfinex.com/v1/ticker/btcusd")
     jsonresponse = json.loads(response.text)
     return jsonresponse
-print("The price is",checksite()["mid"])
 
-message = smsalert.client.messages \
-                .create(
-                     body="The Bitcoin price is now....",
+def is_price_too_high(threshold):
+    """Returns whether the price is above a certain threshold or not, boolean"""
+    price=checksite()["mid"]
+    price=float(price)
+    if price > threshold:
+        result = True
+    else: result = False
+    return result
+
+def send_sms():
+    print("Sending SMS")
+    message = smsalert.client.messages \
+                    .create(
+                         body="The Bitcoin price is now....",
                      from_= secrets.twilio_phone,
                      to= secrets.my_phone
                  )
 
-print(message.sid)
+    return(message.sid)
+
+while True:
+  print("Checking prices")
+  time.sleep(5)
+  if is_price_too_high(40000) == True:
+      send_sms()
+      sys.exit()
+  else: print("The price is not too high")
+
+# check price, if lower than limit, sleep and then check again
+# if higher than limit send message and exit loop
+
+
