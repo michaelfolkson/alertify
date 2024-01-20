839a968a95df4eae9ac0f98d04781b6f7a7c6ff0
Michael Folkson <michaelfolkson@gmail.com>
2024-01-20 13:49:53 +0000

    Ensure the mempool space API is repeatedly called until tx_count doesn't match threshold

--- address_monitor2.py
+++ address_monitor2.py
@@ -7,74 +7,51 @@ import secrets
 import time
 import sys
 
-def checksite():
-    """Gets latest data on the mempool"""
-    response = requests.get("https://blockstream.info/api/mempool")
-    jsonresponse = json.loads(response.text)
-    return jsonresponse
-
-mempool = checksite()
-# print(mempool)
-
-# blockheight = requests.get("https://blockstream.info/api/blocks/tip/height")
-# print(blockheight.text)
-
-
 url = "https://mempool.space/signet/api/address/"
 
 # or "https://mempool.space/api/address/"
 # or "https://blockstream.info/api/address/"
 
 addresses = ["tb1qusjgdyt0d6rw0lh0xdqevpa3aste4j9n4cj4y6", "tb1qdfnge99t2p7a3hpeg43v26wex799jea92fyspn", "tb1qewg2v9pm4sdh98lmzx0fpzalg8ktuq4mf0c50m"]
-thresholds = [0, -1, 0]
+thresholds = [1, 0, 0]
 
 thresholds_dict = dict(zip(addresses, thresholds))
 print(thresholds_dict)
 
-def checkaddress(url, address):
-    """Get the latest tx count of address"""
-    response = requests.get(url + address)
-    jsonresponse = json.loads(response.text)
-    return jsonresponse
-
 tx_counts = []
 
-for address in addresses:
-	address_info = checkaddress(url, address)
-	tx_count = (address_info["chain_stats"]["tx_count"])
-	tx_counts.append(tx_count)
+def checkaddresses(url, addresses):
+        i=0
+        while i < len(addresses):
+            response = requests.get(url + addresses[i])
+            jsonresponse = json.loads(response.text)
+            tx_count = jsonresponse["chain_stats"]["tx_count"]
+            tx_counts.append(tx_count)
+            i = i+1
+        return tx_counts
 
-print(tx_counts)
+tx_count_list = checkaddresses(url, addresses)
 
-tx_count_dict = dict(zip(addresses, tx_counts))
-
-z = []
-if tx_count_dict == thresholds_dict:
-	print("There hasn't been a transaction")
-else:
+def is_tx_count_different(addresses, tx_count_list, thresholds_dict):
+    tx_count_dict = dict(zip(addresses, tx_count_list))
 # Comparing two dictionaries
     set_1 = set(tx_count_dict.items())
     set_2 = set(thresholds_dict.items())
     x = set_1 - set_2
     y = dict(x)
     z = list(y)
-    print(z)
+    return(z)
 
-# def is_tx_count_different(threshold):
-#    """Returns whether the tx count is above a certain threshold or not, boolean"""
-#    if tx_count > threshold`
-#
-#        result = True
-#    else: result = False
-#    return result
+address_diff_list = is_tx_count_different(addresses, tx_count_list, thresholds_dict)
+print(address_diff_list)
 
-i = ""
+j = ""
 
-def send_sms(i):
+def send_sms(j):
     print("Sending SMS")
     message = smsalert.client.messages \
                     .create(
-                         body="There has been a transaction at " + i,
+                         body="There has been a transaction at " + j,
                     from_= secrets.twilio_phone,
                     to= secrets.my_phone
                  )
@@ -82,13 +59,15 @@ def send_sms(i):
     return(message.sid)
 
 while True:
-  print("Checking transactions at addresses")
   time.sleep(5)
-  if len(z) != 0:
-      j=0
-      while j < len(z):
-          send_sms(z[j])
-          j = j+1
+  print("Checking transactions at addresses")
+  tx_count_list = checkaddresses(url, addresses)
+  address_diff_list = is_tx_count_different(addresses, tx_count_list, thresholds_dict)
+  if len(address_diff_list) != 0:
+      k=0
+      while k < len(address_diff_list):
+          send_sms(address_diff_list[k])
+          k = k+1
       sys.exit()
   else: print("No new transactions at addresses")
 
