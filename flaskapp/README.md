d9ab28482eb06b1a52a529ab1ac9a9581cc4673e
Michael Folkson <michaelfolkson@gmail.com>
2024-01-24 10:21:32 +0000

    Add a confirm page once submitted an address

---
+++ flaskapp/README.md
@@ -0,0 +1,7 @@
+To run the Flask web server at http://127.0.0.1:5000:
+
+`flask run`
+
+Or in debug mode:
+
+`flask --debug run`
--- flaskapp/app.py
+++ flaskapp/app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/confirm")
+def confirm():
+    return render_template("confirm.html", name=request.args.get("bitcoin address"))
