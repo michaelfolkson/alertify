c8d94d20c775a85aaf3bb5b72b200e0816dbab53
Michael Folkson <michaelfolkson@gmail.com>
2024-02-04 13:35:02 +0000

    Add Flask layouts and attempt to present list of Bitcoin addresses being monitored

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -2,11 +2,22 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+ADDRESSES = []
+# Should really be a dictionary with username and Bitcoin address
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# Ensuring form input updates a list/dictionary
+# https://youtu.be/oVA0fD13NGI?t=4201
+
 @app.route("/confirm")
 def confirm():
+    name = request.form.get("name")
+    ADDRESSES = name
     return render_template("confirm.html", name=request.args.get("bitcoin address"))
+
+@app.route("/addresses")
+def addresses():
+    return render_template("addresses.html", addresses=ADDRESSES)
